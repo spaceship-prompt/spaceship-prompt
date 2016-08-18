8e5a22d55c1a0055a4e610c8e6b56f119f9f38bd
lbyte <lior.schindler@gmail.com>
2016-08-18 16:41:48 +0300

    Fix for autocompletions breaking git symbols (#27)

--- spaceship.zsh
+++ spaceship.zsh
@@ -152,7 +152,7 @@ spaceship_git_status() {
     echo -n "$(git_current_branch)"
     echo -n "%{$reset_color%}"
     echo -n "%{$fg_bold[red]%}"
-    echo -n "%{$indicators%}"
+    echo -n "$indicators"
     echo -n "%{$reset_color%}"
   fi
 }
