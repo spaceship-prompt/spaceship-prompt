8f428f05a252ce3afa0e9b0551fcc82cf9a79402
Denys Dovhan <email@denysdovhan.com>
2017-05-03 10:30:30 +0300

    Wrap preparations in spaceship_setup function

--- spaceship.zsh
+++ spaceship.zsh
@@ -663,7 +663,6 @@ _deprecated SPACESHIP_GIT_UNPUSHED SPACESHIP_GIT_STATUS_AHEAD
 # An entry point of prompt
 # ------------------------------------------------------------------------------
 
-# Entry point
 # Compose whole prompt from smaller parts
 spaceship_prompt() {
   # Option EXTENDED_GLOB is set locally to force filename generation on
@@ -686,16 +685,25 @@ spaceship_ps2() {
   _prompt_section "yellow" $SPACESHIP_PROMPT_SYMBOL
 }
 
-# Disable python virtualenv environment prompt prefix
-VIRTUAL_ENV_DISABLE_PROMPT=true
+# Setup required environment variables
+# All preparation before drawing prompt should be done here
+spaceship_setup() {
+  # Disable python virtualenv environment prompt prefix
+  VIRTUAL_ENV_DISABLE_PROMPT=true
 
-# Expose Spaceship to environment variables
-PROMPT='$(spaceship_prompt)'
-PS2='$(spaceship_ps2)'
+  # Expose Spaceship to environment variables
+  PROMPT='$(spaceship_prompt)'
+  PS2='$(spaceship_ps2)'
 
-# LSCOLORS
-export LSCOLORS="Gxfxcxdxbxegedabagacab"
-export LS_COLORS='no=00:fi=00:di=01;34:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=41;33;01:ex=00;32:ow=0;41:*.cmd=00;32:*.exe=01;32:*.com=01;32:*.bat=01;32:*.btm=01;32:*.dll=01;32:*.tar=00;31:*.tbz=00;31:*.tgz=00;31:*.rpm=00;31:*.deb=00;31:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.lzma=00;31:*.zip=00;31:*.zoo=00;31:*.z=00;31:*.Z=00;31:*.gz=00;31:*.bz2=00;31:*.tb2=00;31:*.tz2=00;31:*.tbz2=00;31:*.avi=01;35:*.bmp=01;35:*.fli=01;35:*.gif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mng=01;35:*.mov=01;35:*.mpg=01;35:*.pcx=01;35:*.pbm=01;35:*.pgm=01;35:*.png=01;35:*.ppm=01;35:*.tga=01;35:*.tif=01;35:*.xbm=01;35:*.xpm=01;35:*.dl=01;35:*.gl=01;35:*.wmv=01;35:*.aiff=00;32:*.au=00;32:*.mid=00;32:*.mp3=00;32:*.ogg=00;32:*.voc=00;32:*.wav=00;32:*.patch=00;34:*.o=00;32:*.so=01;35:*.ko=01;31:*.la=00;33'
-# Zsh to use the same colors as ls
-# Link: http://superuser.com/a/707567
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+  # LSCOLORS
+  # Online editor: https://geoff.greer.fm/lscolors/
+  export LSCOLORS="Gxfxcxdxbxegedabagacab"
+  export LS_COLORS='no=00:fi=00:di=01;34:ln=00;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=41;33;01:ex=00;32:ow=0;41:*.cmd=00;32:*.exe=01;32:*.com=01;32:*.bat=01;32:*.btm=01;32:*.dll=01;32:*.tar=00;31:*.tbz=00;31:*.tgz=00;31:*.rpm=00;31:*.deb=00;31:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.lzma=00;31:*.zip=00;31:*.zoo=00;31:*.z=00;31:*.Z=00;31:*.gz=00;31:*.bz2=00;31:*.tb2=00;31:*.tz2=00;31:*.tbz2=00;31:*.avi=01;35:*.bmp=01;35:*.fli=01;35:*.gif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mng=01;35:*.mov=01;35:*.mpg=01;35:*.pcx=01;35:*.pbm=01;35:*.pgm=01;35:*.png=01;35:*.ppm=01;35:*.tga=01;35:*.tif=01;35:*.xbm=01;35:*.xpm=01;35:*.dl=01;35:*.gl=01;35:*.wmv=01;35:*.aiff=00;32:*.au=00;32:*.mid=00;32:*.mp3=00;32:*.ogg=00;32:*.voc=00;32:*.wav=00;32:*.patch=00;34:*.o=00;32:*.so=01;35:*.ko=01;31:*.la=00;33'
+  # Zsh to use the same colors as ls
+  # Link: http://superuser.com/a/707567
+  zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+}
+
+# Entry point
+# Pass all arguments to the spaceship_setup function
+spaceship_setup "$@"
