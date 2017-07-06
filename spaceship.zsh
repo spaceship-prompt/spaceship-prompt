ae856960d2b99adda3989922475e6e0ae9b3ef69
Rolf Koenders <post@madebyrolf.nl>
2017-07-06 19:11:14 +0200

    Added some comments

--- spaceship.zsh
+++ spaceship.zsh
@@ -877,16 +877,18 @@ spaceship_docker() {
 # Shows selected aws cli profile.
 spaceship_aws() {
   [[ $SPACESHIP_AWS_SHOW == false ]] && return
+  # Check if the AWS-cli is installed
   _exists aws || return
+  # Is the current profile not the default profile
   [[ -z $AWS_DEFAULT_PROFILE ]] || [[ "$AWS_DEFAULT_PROFILE" == "default" ]] && return
   
+  # Show prompt section
   _prompt_section \
     "$SPACESHIP_AWS_COLOR" \
     "$SPACESHIP_AWS_PREFIX" \
     "${SPACESHIP_AWS_SYMBOL}$AWS_DEFAULT_PROFILE" \
     "$SPACESHIP_AWS_SUFFIX"
 }
-
 
 # VENV
 # Show current virtual environment (Python).
