1c9d078337989cf474f3472129bb4d3cd936504e
Denys Dovhan <email@denysdovhan.com>
2017-09-24 16:20:12 +0300

    Move utils to separate file

--- spaceship.zsh
+++ spaceship.zsh
@@ -65,101 +65,13 @@ SPACESHIP_PROMPT_SUFFIXES_SHOW="${SPACESHIP_PROMPT_SUFFIXES_SHOW:=true}"
 SPACESHIP_PROMPT_DEFAULT_PREFIX="${SPACESHIP_PROMPT_DEFAULT_PREFIX:="via "}"
 SPACESHIP_PROMPT_DEFAULT_SUFFIX="${SPACESHIP_PROMPT_DEFAULT_SUFFIX:=" "}"
 
-
-# ------------------------------------------------------------------------------
-# HELPERS
-# Helpers for common used actions
-# ------------------------------------------------------------------------------
-
-# Check if command exists in $PATH
-# USAGE:
-#   _exists <command>
-_exists() {
-  command -v $1 > /dev/null 2>&1
-}
-
-# Check if the current directory is in a Git repository.
-# USAGE:
-#   _is_git
-_is_git() {
-  command git rev-parse --is-inside-work-tree &>/dev/null
-}
-
-# Check if the current directory is in a Mercurial repository.
-# USAGE:
-#   _is_hg
-_is_hg() {
-  local root="$(pwd -P)"
-
-  while [[ $root && ! -d $root/.hg ]]
-  do
-    root="${root%/*}"
-  done
-
-  [[ -n "$root" ]] &>/dev/null
-}
-
-# Draw prompt section (bold is used as default)
-# USAGE:
-#   _prompt_section <color> [prefix] <content> [suffix]
-SPACESHIP_OPENED=false # Internal variable for checking if prompt is opened
-_prompt_section() {
-  local color prefix content suffix
-  [[ -n $1 ]] && color="%F{$1}"  || color="%f"
-  [[ -n $2 ]] && prefix="$2"     || prefix=""
-  [[ -n $3 ]] && content="$3"    || content=""
-  [[ -n $4 ]] && suffix="$4"     || suffix=""
-
-  [[ -z $3 && -z $4 ]] && content=$2 prefix=''
-
-  echo -n "%{%B%}" # set bold
-  if [[ $SPACESHIP_OPENED == true ]] && [[ $SPACESHIP_PROMPT_PREFIXES_SHOW == true ]]; then
-    echo -n "$prefix"
-  fi
-  SPACESHIP_OPENED=true
-  echo -n "%{%b%}" # unset bold
-
-  echo -n "%{%B$color%}" # set color
-  echo -n "$content"     # section content
-  echo -n "%{%b%f%}"     # unset color
-
-  echo -n "%{%B%}" # reset bold, if it was diabled before
-  if [[ $SPACESHIP_PROMPT_SUFFIXES_SHOW == true ]]; then
-    echo -n "$suffix"
-  fi
-  echo -n "%{%b%}" # unset bold
-}
-
-# Print message backward compatibility warning
-# USAGE:
-#  _deprecate <deprecated> <actual>
-_deprecated() {
-  [[ -n $1 && -n $2 ]] || return
-  local deprecated=$1 actual=$2 b=$bold_color r=$reset_color
-  local deprecated_value=${(P)deprecated} # the value of variable name $deprecated
-  [[ -n $deprecated_value ]] || return
-  echo "${b}\$$deprecated${r} is deprecated. Use ${b}\$$actual${r} instead."
-}
-
-# Display seconds in human readable fromat
-# Based on http://stackoverflow.com/a/32164707/3859566
-# USAGE:
-#   _displaytime <seconds>
-_displaytime() {
-  local T=$1
-  local D=$((T/60/60/24))
-  local H=$((T/60/60%24))
-  local M=$((T/60%60))
-  local S=$((T%60))
-  [[ $D > 0 ]] && printf '%dd ' $D
-  [[ $H > 0 ]] && printf '%dh ' $H
-  [[ $M > 0 ]] && printf '%dm ' $M
-  printf '%ds' $S
-}
+# Load utils
+source "$SPACESHIP_ROOT/utils.zsh"
 
 # ------------------------------------------------------------------------------
 # HOOKS
 # ZSH hooks for advanced actions
+# custom hooks for sections
 # ------------------------------------------------------------------------------
 
 # Execution time start
