0fd77a66bdce433146a10bccd9bf11f6c602b33c
Denys Dovhan <email@denysdovhan.com>
2017-09-24 16:19:24 +0300

    Fix package section. Duplicate of #209

--- sections/package.zsh
+++ sections/package.zsh
@@ -17,7 +17,7 @@ spaceship_package() {
   _exists npm || return
 
   # Grep and cut out package version
-  local package_version=$(grep '"version":' package.json | cut -d\" -f4 2> /dev/null)
+  local package_version=$(grep -E '"version": "v?([0-9]+\.){1,}' package.json | cut -d\" -f4 2> /dev/null)
 
   # Handle version not found
   if [ ! "$package_version" ]; then
