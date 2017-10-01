1566ed6509a5a52987a2789a72cf199d5d8ac253
Denys Dovhan <email@denysdovhan.com>
2017-10-01 17:47:00 +0300

    Update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,103 +1,84 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 #
 # Author: Denys Dovhan, denysdovhan.com
 # https://github.com/denysdovhan/spaceship-zsh-theme
-
-# @TODO: rewrite with sh
-
-# Source ~/.zshrc
-source "$HOME/.zshrc"
 
 # ------------------------------------------------------------------------------
 # HELPERS
 # Useful functions for common tasks
 # ------------------------------------------------------------------------------
 
-# Check if program exists
-# USAGE:
-#   exists <program>
-exists() {
-  command -v $1 >/dev/null 2>&1
-}
+# If we have tput, let's set colors
+if [[ ! -z $(which tput 2> /dev/null) ]]; then
+  reset=$(tput sgr0)
+  bold=$(tput bold)
+  red=$(tput setaf 1)
+  green=$(tput setaf 2)
+  yellow=$(tput setaf 3)
+  blue=$(tput setaf 4)
+  magenta=$(tput setaf 5)
+  cyan=$(tput setaf 6)
+fi
 
-# Paint text in color
-# USAGE:
-#   paint <color> [text...]
 paint() {
-  echo "$fg[$1]${@:2}$reset_color"
+  local color=$1 rest=${@:2}
+  echo "$color$rest$reset"
 }
-
-# Logging functions.
-# USAGE:
-#   log|error|success [text...]
-log()     { echo ; paint 'cyan'  "SPACESHIP: $*" }
-error()   { echo ; paint 'red'   "SPACESHIP: $*" }
-success() { echo ; paint 'green' "SPACESHIP: $*" ; echo }
 
 # ------------------------------------------------------------------------------
 # VARIABLES
 # Paths to important resources
 # ------------------------------------------------------------------------------
 
-URL='https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh'
-SPACESHIP="$PWD/spaceship.zsh"
+REPO='https://github.com/denysdovhan/spaceship-zsh-theme.git'
+SOURCE="$PWD/spaceship.zsh"
+DEST='/usr/local/share/zsh/site-functions'
 
 # ------------------------------------------------------------------------------
 # MAIN
 # Checkings and installing process
 # ------------------------------------------------------------------------------
 
-# Should we download Spaceship form GitHub
-if [ ! -f "$SPACESHIP" ]; then
-  SPACESHIP=$(mktemp /tmp/spaceship.zsh.XXXXXX)
-  log "Spaceship is not present in current directory"
-  log "Temporary file is created: $SPACESHIP"
+# Single-line installation
+if [[ ! -f "$SOURCE" ]]; then
+  paint $yellow"Spaceship is not present in current directory"
 
-  if exists curl; then
-    log "Downloading using curl:"
-    curl "$URL" -o "$SPACESHIP" || $(error "Failed to load using curl!" && exit 1)
-  elif exists wget; then
-    log "Downloading using wget:"
-    wget "$URL" -O "$SPACESHIP" || $(error "Failed to load using wget!" && exit 1)
-  else
-    # Exit with error
-    error "curl and wget are unavailable!"
-    exit 1
-  fi
+  git clone "$REPO" "$HOME/.spaceship-zsh-theme"
 
-  DOWNLOADED=true
+  SOURCE="$HOME/.spaceship-zsh-theme/spaceship.zsh"
 else
-  log "Spaceship is present in current directory"
+  paint $cyan "Spaceship is present in current directory"
 fi
 
 # Choose Installation path
+if [[ ! -w "$DEST" ]]; then
+  paint $red "Failed to symlink $SOURCE to $DEST."
+  paint $cyan "Trying to symlink $SOURCE to $HOME/.zfunctions..."
 
-DIST="/usr/local/share/zsh/site-functions"
-if [[ ! -w "$DIST" ]]; then
-  log "Failed to symlink $SPACESHIP to $DIST, Using $HOME/.zfunctions"
-  DIST="$HOME/.zfunctions"
-  log  "Adding $DIST to fpath"
-  echo 'fpath=( "'"$DIST"'" $fpath )' >> "$HOME/.zshrc"
+  DEST="$HOME/.zfunctions"
+
+  paint $cyan "Adding $DEST to fpath..."
+  echo 'fpath=($fpath "'"$DEST"'" )' >> "$HOME/.zshrc"
 fi
 
 # Link prompt to fpath
-log "Linking $SPACESHIP to $DIST/prompt_spaceship_setup..."
-mkdir -p "$(dirname $DIST)"
-ln -sf "$SPACESHIP" "$DIST/prompt_spaceship_setup"
+paint $cyan "Linking $SOURCE to $DEST/prompt_spaceship_setup..."
+mkdir -p "$DEST"
+ln -sf "$SOURCE" "$DEST/prompt_spaceship_setup"
 
-local msg="
-  autoload -U promptinit; promptinit
-  prompt spaceship
-  "
+msg="
+# Set Spaceship ZSH as a prompt
+autoload -U promptinit; promptinit
+prompt spaceship
+"
 
-read -q "choice?Do you want to set prompt now (Y/n) ?
-This will append the following to your .zshrc $msg"
-
-if [[ $choice == "y" ]]; then
-  echo 'autoload -U promptinit; promptinit' >> "$HOME/.zshrc"
-  echo 'prompt spaceship'                   >> "$HOME/.zshrc"
-  success "Done! Please, reload your terminal."
-else
-  error "Please manually update your .zshrc with the following"
-  echo "$msg"
-fi
+{
+  echo "$msg" >> "$HOME/.zshrc"
+} && {
+  paint $green "Done! Please, reload your terminal."
+} || {
+  # catch
+  paint $red "Cannot automatically insert prompt init commands."
+  paint $red "Please insert these line into your ~/.zshrc:"
+  paint $bold "$msg"
+}
