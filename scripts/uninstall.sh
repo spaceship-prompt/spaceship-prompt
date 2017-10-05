12390f77829bc911aab1d99ef4c333dc11e75fd4
Denys Dovhan <email@denysdovhan.com>
2017-10-05 13:41:51 +0300

    Rewrite uninstall.zsh in sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,9 +1,24 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 #
 # Author: Denys Dovhan, denysdovhan.com
 # https://github.com/denysdovhan/spaceship-zsh-theme
 
-# @TODO: rewrite with sh
+# ------------------------------------------------------------------------------
+# Colors
+# Set color variables for colorful output
+# ------------------------------------------------------------------------------
+
+# If we have tput, let's set colors
+if [[ ! -z $(which tput 2> /dev/null) ]]; then
+  reset=$(tput sgr0)
+  bold=$(tput bold)
+  red=$(tput setaf 1)
+  green=$(tput setaf 2)
+  yellow=$(tput setaf 3)
+  blue=$(tput setaf 4)
+  magenta=$(tput setaf 5)
+  cyan=$(tput setaf 6)
+fi
 
 # ------------------------------------------------------------------------------
 # VARIABLES
@@ -11,58 +26,69 @@
 # ------------------------------------------------------------------------------
 
 ZSHRC="$HOME/.zshrc"
-DIST="/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
+USER_SOURCE="$HOME/.spaceship-zsh-theme"
+GLOBAL_DEST="/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
+USER_DEST="$HOME/.zfunctions/prompt_spaceship_setup"
 
 # ------------------------------------------------------------------------------
 # HELPERS
 # Useful functions for common tasks
 # ------------------------------------------------------------------------------
 
-# Paint text in color
+# Paint text in specific color with reset
 # USAGE:
 #   paint <color> [text...]
 paint() {
-  echo "$fg[$1]${@:2}$reset_color"
+  local color=$1 rest=${@:2}
+  echo "$color$rest$reset"
 }
 
-# Logging functions.
+# Aliases for common used colors
+# Colon at the end is required: https://askubuntu.com/a/521942
 # USAGE:
-#   log|error|success [text...]
-log()     {        paint 'cyan'   "SPACESHIP: $*"        }
-warning() {        paint 'yellow' "SPACESHIP: $*"        }
-error()   { echo ; paint 'red'    "SPACESHIP: $*" ; echo }
-success() { echo ; paint 'green'  "SPACESHIP: $*" ; echo }
+#   info|warn|error|success|code [...text]
+info()    { paint "$cyan"   "SPACESHIP: $@" ; }
+warn()    { paint "$yellow" "SPACESHIP: $@" ; }
+error()   { paint "$red"    "SPACESHIP: $@" ; }
+success() { paint "$green"  "SPACESHIP: $@" ; }
+code()    { paint "$bold"   "SPACESHIP: $@" ; }
+
+# Check if symlink is exists and remove it
+# USAGE:
+#   rmln <target>
+rmln() {
+  local target=$1
+  if [[ -L "$target" ]]; then
+    info "Removing $target..."
+    rm -f "$target"
+  fi
+}
 
 # ------------------------------------------------------------------------------
 # MAIN
 # Checkings and uninstalling process
 # ------------------------------------------------------------------------------
 
-# Source ~/.zshrc
-source "$ZSHRC"
-
-# Remove symlink
-if [[ -L "$DIST" ]]; then
-  log "Removing $DIST..."
-  rm -f "$DIST"
+# Remove $GLOBAL_DEST symlink
+if [[ -L "$GLOBAL_DEST" || -L "$USER_DEST" ]]; then
+  rmln "$GLOBAL_DEST"
+  rmln "$USER_DEST"
 else
-  DIST="$HOME/.zfunctions/prompt_spaceship_setup"
-  if [[ -L "DIST" ]]; then
-    log "Removing $DIST..."
-    rm -f "$DIST"
-  else
-    warning '"$DIST" is not present'
-  fi
+  warn "Symlinks to Spaceship are not found."
 fi
 
-# Remove spaceship from .zshrc
+# Remove Spaceship from .zshrc
 if grep -q "spaceship" "$ZSHRC"; then
-  log "Removing Spaceship from $ZSHRC"
-  sed -i '' '/prompt/d' $ZSHRC
-  sed -i '' '/SPACESHIP/d' $ZSHRC
-  sed -i '' '/.zfunctions/d' $ZSHRC
+  info "Removing Spaceship from ~/.zshrc"
+  # Remove enabling statements from ~/.zshrc
+  sed -i '' '/^# Set Spaceship ZSH as a prompt$/d' $ZSHRC
+  sed -i '' '/^autoload -U promptinit; promptinit$/d' $ZSHRC
+  sed -i '' '/^prompt spaceship$/d' $ZSHRC
+  # Remove Spaceship configuration
+  sed -i '' '/^SPACESHIP_.*$/d' $ZSHRC
 else
-  warning "Spaceship configuration not found in $ZSHRC!"
+  warn "Spaceship configuration not found in ~/.zshrc!"
 fi
 
-success "Done! Spaceship is successfuly removed! Please, reload your terminal."
+success "Done! Spaceship installation have to be removed!"
+success "Please, reload your terminal."
