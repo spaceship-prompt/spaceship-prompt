21a921e84fcc94f8970724fda448173909db619f
Denys Dovhan <email@denysdovhan.com>
2017-10-05 15:53:33 +0300

    Create lib folder

--- lib/utils.zsh
+++ lib/utils.zsh
@@ -31,38 +31,6 @@ _is_hg() {
   [[ -n "$root" ]] &>/dev/null
 }
 
-# Draw prompt section (bold is used as default)
-# USAGE:
-#   _prompt_section <color> [prefix] <content> [suffix]
-# Internal variable for checking if prompt is opened
-spaceship_prompt_opened="$SPACESHIP_PROMPT_FIRST_PREFIX_SHOW"
-_prompt_section() {
-  local color prefix content suffix
-  [[ -n $1 ]] && color="%F{$1}"  || color="%f"
-  [[ -n $2 ]] && prefix="$2"     || prefix=""
-  [[ -n $3 ]] && content="$3"    || content=""
-  [[ -n $4 ]] && suffix="$4"     || suffix=""
-
-  [[ -z $3 && -z $4 ]] && content=$2 prefix=''
-
-  echo -n "%{%B%}" # set bold
-  if [[ $spaceship_prompt_opened == true ]] && [[ $SPACESHIP_PROMPT_PREFIXES_SHOW == true ]]; then
-    echo -n "$prefix"
-  fi
-  spaceship_prompt_opened=true
-  echo -n "%{%b%}" # unset bold
-
-  echo -n "%{%B$color%}" # set color
-  echo -n "$content"     # section content
-  echo -n "%{%b%f%}"     # unset color
-
-  echo -n "%{%B%}" # reset bold, if it was diabled before
-  if [[ $SPACESHIP_PROMPT_SUFFIXES_SHOW == true ]]; then
-    echo -n "$suffix"
-  fi
-  echo -n "%{%b%}" # unset bold
-}
-
 # Print message backward compatibility warning
 # USAGE:
 #  _deprecate <deprecated> <actual>
--- spaceship.zsh
+++ spaceship.zsh
@@ -67,29 +67,13 @@ SPACESHIP_PROMPT_DEFAULT_PREFIX="${SPACESHIP_PROMPT_DEFAULT_PREFIX:="via "}"
 SPACESHIP_PROMPT_DEFAULT_SUFFIX="${SPACESHIP_PROMPT_DEFAULT_SUFFIX:=" "}"
 
 # Load utils
-source "$SPACESHIP_ROOT/utils.zsh"
+source "$SPACESHIP_ROOT/lib/utils.zsh"
 
-# ------------------------------------------------------------------------------
-# HOOKS
-# ZSH hooks for advanced actions
-# custom hooks for sections
-# ------------------------------------------------------------------------------
+# load hooks
+source "$SPACESHIP_ROOT/lib/hooks.zsh"
 
-# Execution time start
-spaceship_exec_time_preexec_hook() {
-  [[ $SPACESHIP_EXEC_TIME_SHOW == false ]] && return
-  SPACESHIP_EXEC_TIME_start=$(date +%s)
-}
-
-# Execution time end
-spaceship_exec_time_precmd_hook() {
-  [[ $SPACESHIP_EXEC_TIME_SHOW == false ]] && return
-  [[ -n $SPACESHIP_EXEC_TIME_duration ]] && unset SPACESHIP_EXEC_TIME_duration
-  [[ -z $SPACESHIP_EXEC_TIME_start ]] && return
-  local SPACESHIP_EXEC_TIME_stop=$(date +%s)
-  SPACESHIP_EXEC_TIME_duration=$(( $SPACESHIP_EXEC_TIME_stop - $SPACESHIP_EXEC_TIME_start ))
-  unset SPACESHIP_EXEC_TIME_start
-}
+# load section utils
+source "$SPACESHIP_ROOT/lib/section.zsh"
 
 # ------------------------------------------------------------------------------
 # SECTIONS
