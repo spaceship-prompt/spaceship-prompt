4f9e47da4838fc17bcc0b3c4ab671ac80662e681
Denys Dovhan <email@denysdovhan.com>
2017-10-19 13:47:35 +0300

    Improve trunc of repos

--- sections/dir.zsh
+++ sections/dir.zsh
@@ -11,9 +11,8 @@ SPACESHIP_DIR_SHOW="${SPACESHIP_DIR_SHOW:=true}"
 SPACESHIP_DIR_PREFIX="${SPACESHIP_DIR_PREFIX:="in "}"
 SPACESHIP_DIR_SUFFIX="${SPACESHIP_DIR_SUFFIX:="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_DIR_TRUNC="${SPACESHIP_DIR_TRUNC:=3}"
+SPACESHIP_DIR_TRUNC_REPO="${SPACESHIP_DIR_TRUNC_REPO:=true}"
 SPACESHIP_DIR_COLOR="${SPACESHIP_DIR_COLOR:="cyan"}"
-# @TODO: resolve with git
-SPACESHIP_DIR_TRUNC_IN_GIT="${SPACESHIP_DIR_TRUNC_IN_GIT:=false}"
 
 # ------------------------------------------------------------------------------
 # Section
@@ -22,19 +21,19 @@ SPACESHIP_DIR_TRUNC_IN_GIT="${SPACESHIP_DIR_TRUNC_IN_GIT:=false}"
 spaceship_dir() {
   [[ $SPACESHIP_DIR_SHOW == false ]] && return
 
-  if _is_git && [[ $SPACESHIP_DIR_TRUNC_IN_GIT == true ]]; then
-    GIT_ROOT=$(git rev-parse --show-toplevel)
+  local dir
 
-    _prompt_section \
-      "$SPACESHIP_DIR_COLOR" \
-      "$SPACESHIP_DIR_PREFIX" \
-      "$GIT_ROOT:t${$(expr $(pwd) : "$GIT_ROOT\(.*\)")}" \
-      "$SPACESHIP_DIR_SUFFIX"
+  # Threat repo root as a top-level directory or not
+  if [[ _is_git && $SPACESHIP_DIR_TRUNC_REPO == true ]]; then
+    local git_root=$(git rev-parse --show-toplevel)
+    dir="$git_root:t${$(expr $(pwd) : "$git_root\(.*\)")}"
   else
-    _prompt_section \
-      "$SPACESHIP_DIR_COLOR" \
-      "$SPACESHIP_DIR_PREFIX" \
-      "%${SPACESHIP_DIR_TRUNC}~" \
-      "$SPACESHIP_DIR_SUFFIX"
+    dir="%${SPACESHIP_DIR_TRUNC}~"
   fi
+
+  _prompt_section \
+    "$SPACESHIP_DIR_COLOR" \
+    "$SPACESHIP_DIR_PREFIX" \
+    "$dir" \
+    "$SPACESHIP_DIR_SUFFIX"
 }
