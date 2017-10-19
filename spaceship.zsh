13912054232beac931b0d3b5b6c0adf1205b3aa9
Denys Dovhan <email@denysdovhan.com>
2017-10-19 20:58:01 +0300

    Add comments to sources

--- spaceship.zsh
+++ spaceship.zsh
@@ -41,38 +41,38 @@ fi
 # ORDER
 if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
   SPACESHIP_PROMPT_ORDER=(
-    time
-    user
-    host
-    dir
-    git
-    hg
-    package
-    node
-    ruby
-    elixir
-    xcode
-    swift
-    golang
-    php
-    rust
-    haskell
-    julia
-    docker
-    aws
-    venv
-    conda
-    pyenv
-    dotnet
-    ember
-    kubecontext
-    exec_time
-    line_sep
-    battery
-    vi_mode
-    jobs
-    exit_code
-    char
+    time          # Time stampts section
+    user          # Username section
+    host          # Hostname section
+    dir           # Current directory section
+    git           # Git section (git_branch + git_status)
+    hg            # Mercurial section (hg_branch  + hg_status)
+    package       # Package version
+    node          # Node.js section
+    ruby          # Ruby section
+    elixir        # Elixir section
+    xcode         # Xcode section
+    swift         # Swift section
+    golang        # Go section
+    php           # PHP section
+    rust          # Rust section
+    haskell       # Haskell Stack section
+    julia         # Julia section
+    docker        # Docker section
+    aws           # Amazon Web Services section
+    venv          # virtualenv section
+    conda         # conda virtualenv section
+    pyenv         # Pyenv section
+    dotnet        # .NET section
+    ember         # Ember.js section
+    kubecontext   # Kubectl context section
+    exec_time     # Execution time
+    line_sep      # Line break
+    battery       # Battery level and status
+    vi_mode       # Vi-mode indicator
+    jobs          # Backgound jobs indicator
+    exit_code     # Exit code section
+    char          # Prompt character
   )
 fi
 
