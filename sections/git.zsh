d7767abd84da53fa6aff1182492b359f84238624
Maxim Baz <git@maximbaz.com>
2017-12-13 00:38:28 +0100

    Fix missing git branch prefix
    The `git_branch` dependency requires parent configuration `$SPACESHIP_GIT_SYMBOL` to be set at the time it is sourced.

--- sections/git.zsh
+++ sections/git.zsh
@@ -3,13 +3,6 @@
 #
 
 # ------------------------------------------------------------------------------
-# Dependencies
-# ------------------------------------------------------------------------------
-
-source "$SPACESHIP_ROOT/sections/git_branch.zsh"
-source "$SPACESHIP_ROOT/sections/git_status.zsh"
-
-# ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 
@@ -17,6 +10,13 @@ SPACESHIP_GIT_SHOW="${SPACESHIP_GIT_SHOW:=true}"
 SPACESHIP_GIT_PREFIX="${SPACESHIP_GIT_PREFIX:="on "}"
 SPACESHIP_GIT_SUFFIX="${SPACESHIP_GIT_SUFFIX:="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_GIT_SYMBOL="${SPACESHIP_GIT_SYMBOL:=" "}"
+
+# ------------------------------------------------------------------------------
+# Dependencies
+# ------------------------------------------------------------------------------
+
+source "$SPACESHIP_ROOT/sections/git_branch.zsh"
+source "$SPACESHIP_ROOT/sections/git_status.zsh"
 
 # ------------------------------------------------------------------------------
 # Section
