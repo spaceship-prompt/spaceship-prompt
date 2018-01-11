881e597934dfcace887b51a29db4b868528f3f98
Denys Dovhan <denysdovhan@gmail.com>
2018-01-11 20:20:26 +0200

    Reproduce #263 on #148

--- sections/docker.zsh
+++ sections/docker.zsh
@@ -28,9 +28,8 @@ spaceship_docker() {
   [[ -f Dockerfile || -f docker-compose.yml ]] || return
 
   # if docker daemon isn't running you'll get an error saying it can't connect
-  docker info 2>&1 | grep -q "Cannot connect" && return
-
-  local docker_version=$(docker version -f "{{.Server.Version}}")
+  local docker_version=$(docker version -f "{{.Server.Version}}" 2>/dev/null)
+  [[ -z $docker_version ]] && return
 
   if [[ -n $DOCKER_MACHINE_NAME ]]; then
     docker_version+=" via ($DOCKER_MACHINE_NAME)"
