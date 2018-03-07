53eb8632b8673a7683fb6c4242f743a9fcefc02b
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-03-07 09:06:22 +0530

    Fix dir truncation for .git repo and subdirs

--- sections/dir.zsh
+++ sections/dir.zsh
@@ -25,7 +25,7 @@ spaceship_dir() {
 
   # Threat repo root as a top-level directory or not
   if [[ $SPACESHIP_DIR_TRUNC_REPO == true ]] && spaceship::is_git; then
-    local git_root=$(git rev-parse --show-toplevel)
+    local git_root=${$(git rev-parse --absolute-git-dir):h}
     dir="$git_root:t${$(expr $(pwd -P) : "$git_root\(.*\)")}"
   else
     dir="%${SPACESHIP_DIR_TRUNC}~"
