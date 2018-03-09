46d07f04c7983fc93324f206efdadf330a70f54c
Denys Dovhan <denysdovhan@gmail.com>
2018-03-09 20:14:30 +0200

    3.3.0

--- spaceship.zsh
+++ spaceship.zsh
@@ -7,7 +7,7 @@
 
 # Current version of Spaceship
 # Useful for issue reporting
-export SPACESHIP_VERSION='3.2.0'
+export SPACESHIP_VERSION='3.3.0'
 
 # Common-used variable for new line separator
 NEWLINE='
