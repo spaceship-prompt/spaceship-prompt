c28e9076748bd2400a161fe2fa37ba5138acf3e0
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-04-27 08:07:32 +0530

    Use perl to upgrade version in spaceship
    sed -i isn't POSIX compliant and behaves differently on BSD and GNU
    flavours of sed
    
    BSD sed -i expects an extension after space. While this extension
    is optional on GNU sed and must be on the same argument without any
    intervening space. This breaks cross compatibility of script.

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,6 +3,6 @@
 new_version=$(grep -E '"version": "v?([0-9]+\.){1,}' package.json | cut -d\" -f4 2> /dev/null)
 filename="$PWD/spaceship.zsh"
 
-sed -i '' -e "s/SPACESHIP_VERSION='.*'/SPACESHIP_VERSION='$new_version'/g" $filename
+command perl -i -pe "s/SPACESHIP_VERSION='.*'/SPACESHIP_VERSION='$new_version'/g" $filename
 
 git add spaceship.zsh
