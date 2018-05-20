34352223c3ae9f740d3b5f3aada0054694a94e3a
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-05-20 19:41:06 -0300

    Vagrant status
    We get the status by parsing the `$HOME/.vagrant.d/data/machine-index/index` json file.

--- sections/vagrant.zsh
+++ sections/vagrant.zsh
@@ -27,7 +27,15 @@ spaceship_vagrant() {
   # Show Vagrant status only for Vagrant-specific folders
   [[ -f Vagrantfile || (-n $VAGRANT_VAGRANTFILE && -f ${VAGRANT_VAGRANTFILE}) ]] || return
 
-  local vagrant_status=$(vagrant status | sed -n 3p | awk {'print $2'})
+  # local vagrant_status=$(cat .vagrant/machines/*/virtualbox/id | xargs VboxManage showvminfo | grep State | awk {'print $2'})
+
+  local vagrant_status=$(cat ${HOME}/.vagrant.d/data/machine-index/index | python -c 'import sys, os, json;
+json_file = json.load(sys.stdin)["machines"]
+for box in f:
+  if (json_file[box["vagrantfile_path"] == os.getcwd()):
+    print (json_file[box]["state"])
+    break;
+')
 
   spaceship::section \
     "$SPACESHIP_VAGRANT_COLOR" \
