1ab80a67cdfbdc1bd287c57a931f53ea829eef64
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-05-25 14:16:29 +0530

    Fix typo in git_*_prefix

--- sections/git_branch.zsh
+++ sections/git_branch.zsh
@@ -39,18 +39,18 @@ spaceship_git_branch() {
 
   # Build commit SHA info if enabled
   if [[ $SPACESHIP_GIT_BRANCH_SHOW_COMMIT == true ]]; then
-    local git_commit_preffix="$SPACESHIP_GIT_BRANCH_COMMIT_PREFIX$SPACESHIP_GIT_BRANCH_COMMIT_SYMBOL"
+    local git_commit_prefix="$SPACESHIP_GIT_BRANCH_COMMIT_PREFIX$SPACESHIP_GIT_BRANCH_COMMIT_SYMBOL"
     local git_commit_sha=$(git rev-parse --short HEAD 2> /dev/null)
-    local git_commit_info="$git_commit_preffix$git_commit_sha$SPACESHIP_GIT_BRANCH_COMMIT_SUFFIX"
+    local git_commit_info="$git_commit_prefix$git_commit_sha$SPACESHIP_GIT_BRANCH_COMMIT_SUFFIX"
   fi
 
   git_current_branch+="$git_commit_info"
 
   # Build tag info if enabled
   if [[ $SPACESHIP_GIT_BRANCH_SHOW_TAG == true ]]; then
-    local git_tag_preffix="$SPACESHIP_GIT_BRANCH_TAG_PREFIX$SPACESHIP_GIT_BRANCH_TAG_SYMBOL"
+    local git_tag_prefix="$SPACESHIP_GIT_BRANCH_TAG_PREFIX$SPACESHIP_GIT_BRANCH_TAG_SYMBOL"
     local git_tag_name=$(git describe --tags --exact-match $git_commit_sha 2>/dev/null)
-    local git_tag_info="$git_tag_preffix$git_tag_name$SPACESHIP_GIT_BRANCH_TAG_SUFFIX"
+    local git_tag_info="$git_tag_prefix$git_tag_name$SPACESHIP_GIT_BRANCH_TAG_SUFFIX"
     if [[ -n $git_tag_name ]]; then
       git_current_branch+="$git_tag_info"
     fi
