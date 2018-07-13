da7e90923e302bd8209873d4d7cf005550c3f8d5
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-07-13 18:35:48 -0300

    use jq if available and python as fallback option to parse json

--- sections/vagrant.zsh
+++ sections/vagrant.zsh
@@ -27,13 +27,18 @@ spaceship_vagrant() {
   # Show Vagrant status only for Vagrant-specific folders
   [[ -f Vagrantfile || (-n $VAGRANT_VAGRANTFILE && -f ${VAGRANT_VAGRANTFILE}) ]] || return
 
-  local vagrant_status=$(cat ${HOME}/.vagrant.d/data/machine-index/index | python -c 'import sys, os, json;
+  if spaceship::exists jq || 0; then
+    local vagrant_status=$(cat "$HOME/.vagrant.d/data/machine-index/index" |
+jq -r --arg dir "$PWD" '.machines[] | select(.vagrantfile_path == $dir).state')
+  else
+    local vagrant_status=$(cat ${HOME}/.vagrant.d/data/machine-index/index | python -c 'import sys, os, json;
 json_file = json.load(sys.stdin)["machines"]
 for box in json_file:
   if (json_file[box]["vagrantfile_path"] == os.getcwd()):
     print (json_file[box]["state"])
     break;
 ')
+  fi
 
   [[ -n ${vagrant_status} ]] || return
 
