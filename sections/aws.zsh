2fc0891a57f02c4e0a654a68139b5639bf752db4
Russell Cousineau <miller.time.baby@gmail.com>
2018-08-23 13:05:12 -0700

    Replace AWS_DEFAULT_PROFILE with AWS_PROFILE

--- sections/aws.zsh
+++ sections/aws.zsh
@@ -26,12 +26,12 @@ spaceship_aws() {
   spaceship::exists aws || return
 
   # Is the current profile not the default profile
-  [[ -z $AWS_DEFAULT_PROFILE ]] || [[ "$AWS_DEFAULT_PROFILE" == "default" ]] && return
+  [[ -z $AWS_PROFILE ]] || [[ "$AWS_PROFILE" == "default" ]] && return
 
   # Show prompt section
   spaceship::section \
     "$SPACESHIP_AWS_COLOR" \
     "$SPACESHIP_AWS_PREFIX" \
-    "${SPACESHIP_AWS_SYMBOL}$AWS_DEFAULT_PROFILE" \
+    "${SPACESHIP_AWS_SYMBOL}$AWS_PROFILE" \
     "$SPACESHIP_AWS_SUFFIX"
 }
