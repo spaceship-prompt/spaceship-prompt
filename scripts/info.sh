d7039ade36a36a7a544747b8a82941cd2a9a913a
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-12-02 13:29:37 +0530

    Helpers: Add colors, Minimal uname caching

--- scripts/info.sh
+++ scripts/info.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env zsh
 
+# ------------------------------------------------------------------------------
+# HELPERS
+# Useful functions for common tasks
+# ------------------------------------------------------------------------------
+
+# Paint text in specific color with reset
+# USAGE:
+#   paint <color> [text...]
+paint() {
+  local title=$1 content=${@:2}
+  echo "$title: $content"
+}
+
+cache_uname() {
+  kernel_name="$(uname -s 2>/dev/null)"
+
+    if [[ "$kernel_name" == "Darwin" ]]; then
+        IFS=$'\n' read -d "" -r sw_vers < <(awk -F'<|>' '/string/ {print $3}' \
+                            "/System/Library/CoreServices/SystemVersion.plist")
+        darwin_name="${sw_vers[2]}"
+        osx_version="${sw_vers[3]}"
+        osx_build="${sw_vers[0]}"
+    fi
+}
+
 get_os() {
     # $kernel_name is set in a function called cache_uname and is
     # just the output of "uname -s".
@@ -16,8 +41,7 @@ get_os() {
         "CYGWIN"* | "MSYS"* | "MINGW"*):"Windows" ;;
         *):"$kernel_name" ;;
     esac
-    os="$_"
-    print "${os}"
+    paint "Operating System" $os
 }
 
 get_shell() {
@@ -50,25 +74,7 @@ get_shell() {
     shell="${shell/options*}"
     shell="${shell/\(*\)}"
 
-    print "${shell}"
-}
-
-cache_uname() {
-    # Cache the output of uname so we don't
-    # have to spawn it multiple times.
-    IFS=" " read -r uname <<< "$(uname -srm)"
-
-    kernel_name="${uname[0]}"
-    kernel_version="${uname[1]}"
-    kernel_machine="${uname[2]}"
-
-    if [[ "$kernel_name" == "Darwin" ]]; then
-        IFS=$'\n' read -d "" -r sw_vers < <(awk -F'<|>' '/string/ {print $3}' \
-                            "/System/Library/CoreServices/SystemVersion.plist")
-        darwin_name="${sw_vers[2]}"
-        osx_version="${sw_vers[3]}"
-        osx_build="${sw_vers[0]}"
-    fi
+   paint "Shell" $shell
 }
 
 main() {
