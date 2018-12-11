4a7aaf8769e171909febbd52ae2938525cf808ae
Boris Aranovich <nomaed@users.noreply.github.com>
2018-12-11 14:45:29 +0200

    Custom colors per kubectl context/namespace (#576)
    It allows setting SPACESHIP_KUBECONTEXT_COLOR_GROUPS array with colors 
    and patterns that match context names and namespaces, that should change
    the section to the specified color. 
    
    This is usually going to be critical environments that we need to make 
    sure we don't modify by mistake, such as "production" or "staging" 
    (as opposed to regular "dev").
    
    Co-Authored-By: Maxim Baz <maximbaz@users.noreply.github.com>

--- sections/kubecontext.zsh
+++ sections/kubecontext.zsh
@@ -17,6 +17,7 @@ SPACESHIP_KUBECONTEXT_SUFFIX="${SPACESHIP_KUBECONTEXT_SUFFIX="$SPACESHIP_PROMPT_
 SPACESHIP_KUBECONTEXT_SYMBOL="${SPACESHIP_KUBECONTEXT_SYMBOL="☸️  "}"
 SPACESHIP_KUBECONTEXT_COLOR="${SPACESHIP_KUBECONTEXT_COLOR="cyan"}"
 SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW="${SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW=true}"
+SPACESHIP_KUBECONTEXT_COLOR_GROUPS=(${SPACESHIP_KUBECONTEXT_COLOR_GROUPS=()})
 
 # ------------------------------------------------------------------------------
 # Section
@@ -36,8 +37,24 @@ spaceship_kubecontext() {
     [[ -n $kube_namespace && "$kube_namespace" != "default" ]] && kube_context="$kube_context ($kube_namespace)"
   fi
 
+  # Apply custom color to section if $kube_context matches a pattern defined in SPACESHIP_KUBECONTEXT_COLOR_GROUPS array.
+  # See Options.md for usage example.
+  local 'section_color' 'i' 'color' 'pattern'
+  local len=${#SPACESHIP_KUBECONTEXT_COLOR_GROUPS[@]}
+
+  for ((i = 1; i <= $len; i+=2)); do
+    color="${SPACESHIP_KUBECONTEXT_COLOR_GROUPS[$i]}"
+    pattern="${SPACESHIP_KUBECONTEXT_COLOR_GROUPS[$i+1]}"
+    if [[ "$kube_context" =~ "$pattern" ]]; then
+      section_color=$color
+      break
+    fi
+  done
+
+  [[ -z "$section_color" ]] && section_color=$SPACESHIP_KUBECONTEXT_COLOR
+
   spaceship::section \
-    "$SPACESHIP_KUBECONTEXT_COLOR" \
+    "$section_color" \
     "$SPACESHIP_KUBECONTEXT_PREFIX" \
     "${SPACESHIP_KUBECONTEXT_SYMBOL}${kube_context}" \
     "$SPACESHIP_KUBECONTEXT_SUFFIX"
