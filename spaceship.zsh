eb507d83a63888eb6517ccd8f3a274e6ef560595
Salmanul Farzy <salmanulfarzy@gmail.com>
2019-01-07 11:10:09 +0530

    3.9.1

--- spaceship.zsh
+++ spaceship.zsh
@@ -7,7 +7,7 @@
 
 # Current version of Spaceship
 # Useful for issue reporting
-export SPACESHIP_VERSION='3.9.0'
+export SPACESHIP_VERSION='3.9.1'
 
 # Common-used variable for new line separator
 NEWLINE='
