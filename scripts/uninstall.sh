13149b5ad1bc1158ab850582b356f96258ae7687
Runrioter <runrioter@qq.com>
2019-03-05 20:28:02 +0800

    fix: fix bug

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -80,7 +80,7 @@ main() {
 
   # Remove Spaceship from .zshrc
   if grep -q "spaceship" "$ZSHRC"; then
-    read answer '?Would you like to remove you Spaceship ZSH configuration from .zshrc? (y/N)'
+    read "answer?Would you like to remove you Spaceship ZSH configuration from .zshrc? (y/N)"
     if [[ 'y' == ${answer:l} ]]; then
       info "Removing Spaceship from ~/.zshrc"
       # Remove enabling statements from ~/.zshrc
