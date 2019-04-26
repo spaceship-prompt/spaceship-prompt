5b368be497cbe6e8e1b5dc86e023a4f6ef1d0ad1
Adrien Lemaire <lemaire.adrien@gmail.com>
2019-04-26 18:12:13 +0900

    fix: bug if not equal

--- sections/task.zsh
+++ sections/task.zsh
@@ -28,7 +28,7 @@ spaceship_task() {
 
   local 'task_count'
 
-  if [[ $(task ready) -neq "No matches." ]]; then
+  if [[ $(task ready) != "No matches." ]]; then
     task_count=$(task ready | tail -n1 | awk '{print $1}')
   fi
 
@@ -38,5 +38,5 @@ spaceship_task() {
   # Display task section
   spaceship::section \
     "$SPACESHIP_TASK_COLOR" \
-    "$SPACESHIP_TASK_SYMBOL$task_count" \
+    "$SPACESHIP_TASK_SYMBOL $task_count" \
   }
