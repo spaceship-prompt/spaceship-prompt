8bd11bc4ac6d770b46cd22e71a13012746ef2f60
Estelle Poulin <dev@inspiredby.es>
2019-04-29 10:28:37 -0400

    Add Sudo section

---
+++ sections/sudo.zsh
@@ -0,0 +1,47 @@
+#
+# Sudo
+#
+# Sudo is a tool that allows a system administrator to delegate authority to
+# users or groups.
+# Link: https://www.sudo.ws
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_SUDO_SHOW="${SPACESHIP_SUDO_SHOW=true}"
+SPACESHIP_SUDO_PREFIX="${SPACESHIP_SUDO_PREFIX="with "}"
+SPACESHIP_SUDO_SUFFIX="${SPACESHIP_SUDO_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_SUDO_SYMBOL="${SPACESHIP_SUDO_SYMBOL="🗲 "}"
+SPACESHIP_SUDO_COLOR="${SPACESHIP_SUDO_COLOR="yellow"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# Show sudo status
+# spaceship_ prefix before section's name is required!
+# Otherwise this section won't be loaded.
+spaceship_sudo() {
+  # If SPACESHIP_SUDO_SHOW is false, don't show sudo section
+  [[ $SPACESHIP_SUDO_SHOW == false ]] && return
+
+  # Check if sudo command is available for execution
+  spaceship::exists sudo || return
+
+  local 'sudo_status'
+
+  if sudo -nl 2>&1 >/dev/null; then
+      sudo_status='sudo'
+  fi
+
+  # Exit section if variable is empty
+  [[ -z $sudo_status ]] && return
+
+  # Display sudo section
+  spaceship::section \
+    "$SPACESHIP_SUDO_COLOR" \
+    "$SPACESHIP_SUDO_PREFIX" \
+    "$SPACESHIP_SUDO_SYMBOL$sudo_status" \
+    "$SPACESHIP_SUDO_SUFFIX"
+}
