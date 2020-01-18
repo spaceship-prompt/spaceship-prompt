eb77de201a6f9de47fdf07d3658abd66591a126b
Leonardo Villela <leonardo.martins@talkdesk.com>
2020-01-18 22:03:36 +0000

    Add uninstall test

---
+++ tests/uninstall.test.zsh
@@ -0,0 +1,52 @@
+#!/usr/bin/env zsh
+
+# Required for shunit2 to run correctly
+setopt shwordsplit
+SHUNIT_PARENT=$0
+
+# ------------------------------------------------------------------------------
+# SHUNIT2 HOOKS
+# ------------------------------------------------------------------------------
+
+oneTimeSetUp() {
+  export TERM="xterm-256color"
+
+  SPACESHIP_PROMPT_FIRST_PREFIX_SHOW=true
+  SPACESHIP_PROMPT_ADD_NEWLINE=false
+}
+
+setUp() {
+  echo "" > ~/.zshrc
+  ./scripts/install.sh >/dev/null
+}
+
+# ------------------------------------------------------------------------------
+# TEST CASES
+# ------------------------------------------------------------------------------
+
+test_uninstall_preserve_zshrc_content() {
+  echo "TEST=TEST" >> ~/.zshrc
+
+  ./scripts/uninstall.sh -y >/dev/null
+
+  zshrc_content=$(<~/.zshrc)
+
+  assertContains "preserve non spaceship related install config" "$zshrc_content" "TEST=TEST"
+}
+
+test_uninstall_remove_spaceship_content_from_zshrc() {
+  ./scripts/uninstall.sh -y >/dev/null
+
+  zshrc_content=$(<~/.zshrc)
+
+  assertNotContains "remove spaceship load comment" "$zshrc_content" "# Set Spaceship ZSH as a prompt"
+  assertNotContains "remove auto load prompt init" "$zshrc_content" "autoload -U promptinit; promptinit"
+  assertNotContains "remove SPACESHIP_PROMPT_ORDER env" "$zshrc_content" "SPACESHIP_RPROMPT_ORDER"
+}
+
+# ------------------------------------------------------------------------------
+# SHUNIT2
+# Run tests with shunit2
+# ------------------------------------------------------------------------------
+
+source modules/shunit2/shunit2
