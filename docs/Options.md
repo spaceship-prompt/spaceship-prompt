c5238797c76c78c9f7901c2586785eabf867517b
Carlos Esquerdo Bernat <esquerbatua@gmail.com>
2020-11-05 22:29:08 +0100

    V Section

--- docs/Options.md
+++ docs/Options.md
@@ -649,6 +649,18 @@ Disabled by default. Set `SPACESHIP_EXIT_CODE_SHOW` to `true` in your `.zshrc`, 
 | `SPACESHIP_EXIT_CODE_SYMBOL` | `✘` | Character to be shown before exit code |
 | `SPACESHIP_EXIT_CODE_COLOR` | `red` | Color of exit code section |
 
+### V (`v`)
+
+V section is shown only in directories that contain `*.v*` extension.
+
+| Variable | Default | Meaning |
+| :------- | :-----: | ------- |
+| `SPACESHIP_V_SHOW` | `true` | Current V section |
+| `SPACESHIP_V_PREFIX` | `$SPACESHIP_PROMPT_DEFAULT_PREFIX` | Prefix before V section |
+| `SPACESHIP_V_SUFFIX` | `$SPACESHIP_PROMPT_DEFAULT_SUFFIX` | Suffix after V section |
+| `SPACESHIP_V_SYMBOL` | `V(bold cyan) ` | Character to be shown before V version |
+| `SPACESHIP_V_COLOR` | `blue` | Color of V section |
+
 ## Need more?
 
 If these options are not enough to do what you want, read more about Spaceship's API on [API page](./API.md) of the documentation.
---
+++ sections/v.zsh
@@ -0,0 +1,41 @@
+#
+# V
+#
+# V is a languaje program designed to be "Simple, fast, safe, compiled. For developing maintainable software."
+# Link: https://vlang.io/
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_V_SHOW="${SPACESHIP_V_SHOW=true}"
+SPACESHIP_V_PREFIX="${SPACESHIP_V_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
+SPACESHIP_V_SUFFIX="${SPACESHIP_V_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_V_SYMBOL="${SPACESHIP_V_SYMBOL="V(bold cyan) "}"
+SPACESHIP_V_COLOR="${SPACESHIP_V_COLOR="blue"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# Show current version of V, exception system.
+spaceship_v() {
+  [[ $SPACESHIP_V_SHOW == false ]] && return
+
+  # Show V status only for folders containing V files
+  [[ -n *.v(#qN^/) ]] || return
+
+  local 'v_version'
+
+  if spaceship::exists v; then
+    v_version=$(v version)
+  else
+    return
+  fi
+
+  spaceship::section \
+    "$SPACESHIP_V_COLOR" \
+    "$SPACESHIP_V_PREFIX" \
+    "${SPACESHIP_V_SYMBOL}${v_version}" \
+    "$SPACESHIP_V_SUFFIX"
+}
