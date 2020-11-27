dc89d637dca43ca33d578ed6741dcea314a57ecd
Raleigh Littles <raleighlittles@gmail.com>
2020-11-26 23:24:21 -0800

    1st commit. Added new section.

--- sections/git.zsh
+++ sections/git.zsh
@@ -17,18 +17,20 @@ SPACESHIP_GIT_SYMBOL="${SPACESHIP_GIT_SYMBOL=" "}"
 
 source "$SPACESHIP_ROOT/sections/git_branch.zsh"
 source "$SPACESHIP_ROOT/sections/git_status.zsh"
+source "$SPACESHIP_ROOT/sections/git_commit_msg.zsh"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-# Show both git branch and git status:
+# Show both git branch, status, and commit msg:
 #   spaceship_git_branch
 #   spaceship_git_status
+#   spaceship_git_commit_msg
 spaceship_git() {
   [[ $SPACESHIP_GIT_SHOW == false ]] && return
 
-  local git_branch="$(spaceship_git_branch)" git_status="$(spaceship_git_status)"
+  local git_branch="$(spaceship_git_branch)" git_status="$(spaceship_git_status)" git_commit_msg="$(spaceship_git_commit_msg)"
 
   [[ -z $git_branch ]] && return
 
@@ -36,5 +38,6 @@ spaceship_git() {
     'white' \
     "$SPACESHIP_GIT_PREFIX" \
     "${git_branch}${git_status}" \
+    "${git_commit_msg}" \
     "$SPACESHIP_GIT_SUFFIX"
 }
