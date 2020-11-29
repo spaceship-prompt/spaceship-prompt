efa341df9010e5f376ff363b77ad5783251ede19
Raleigh Littles III <raleighlittles@gmail.com>
2020-11-28 20:47:41 -0800

    Added 3 new sections
    1) git_tag, which shows the latest tag \n 2) git_commit_msg, which shows the commit message of the last commit [off by default] \n 3) git_diffstat, which pprints the git diff statistics (lines added/removed)

---
+++ sections/git_diffstat.zsh
@@ -0,0 +1,34 @@
+
+#
+# Prints line counts of diff (added and removed)
+#
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_GIT_DIFFSTAT_SHOW="${SPACESHIP_GIT_DIFFSTAT_SHOW=true}"
+SPACESHIP_GIT_DIFFSTAT_PREFIX="${SPACESHIP_GIT_DIFFSTAT_PREFIX="\t"}"
+SPACESHIP_GIT_DIFFSTAT_SUFFIX="${SPACESHIP_GIT_DIFFSTAT_SUFFIX=""}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+spaceship_git_diffstat() {
+  [[ $SPACESHIP_GIT_DIFFSTAT_SHOW == false ]] && return
+
+  spaceship::is_git || return
+
+  # Shows count of lines added in green (with '+' prefix) and lines removed in red (with '-' prefix)
+  # e.g. "+21 -7"
+  
+  git_diffstat=$(command git diff --numstat HEAD | awk 'NF==3 {added+=$1; removed+=$2} END {printf("\x1B[32m+%d\x1B[0m \x1B[31m-%d\x1B[0m\n", added, removed)}')
+
+  if [[ -n $(git status -s) ]]; then
+    # Commit prefixes are colorized
+    spaceship::section \
+      '' \
+      "$SPACESHIP_GIT_DIFFSTAT_PREFIX$git_diffstat$SPACESHIP_GIT_DIFFSTAT_SUFFIX"
+  fi
+}
---
+++ sections/git_tag.zsh
@@ -0,0 +1,32 @@
+#
+# Git tag
+#
+# Show last git tag
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+	
+SPACESHIP_GIT_TAG_SHOW="${SPACESHIP_GIT_TAG_SHOW=true}"
+SPACESHIP_GIT_TAG_PREFIX="${SPACESHIP_GIT_TAG_PREFIX=" @"}"
+SPACESHIP_GIT_TAG_SUFFIX="${SPACESHIP_GIT_TAG_SUFFIX=""}"
+SPACESHIP_GIT_TAG_COLOR="${SPACESHIP_GIT_TAG_COLOR="chartreuse"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+spaceship_git_tag() {
+  [[ $SPACESHIP_GIT_TAG_SHOW == false ]] && return
+
+  spaceship::is_git || return
+
+  git_tag=$(command git describe --tags --abbrev=0)
+
+  if [[ $? -eq 0 ]]; then
+    spaceship::section \
+    "$SPACESHIP_GIT_TAG_COLOR" \
+    "$SPACESHIP_GIT_TAG_PREFIX$git_tag$SPACESHIP_GIT_TAG_SUFFIX"
+  fi
+}
