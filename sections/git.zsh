2970a38ddc1eb3fe8d0122ae3eab1132e84b1a1c
Kasper <kasperliu129@gmail.com>
2021-04-19 16:05:19 +0800

    Initial Commit

--- sections/git.zsh
+++ sections/git.zsh
@@ -16,6 +16,7 @@ SPACESHIP_GIT_SYMBOL="${SPACESHIP_GIT_SYMBOL=" "}"
 # ------------------------------------------------------------------------------
 
 source "$SPACESHIP_ROOT/sections/git_branch.zsh"
+source "$SPACESHIP_ROOT/sections/git_stat.zsh"
 source "$SPACESHIP_ROOT/sections/git_status.zsh"
 
 # ------------------------------------------------------------------------------
@@ -28,13 +29,15 @@ source "$SPACESHIP_ROOT/sections/git_status.zsh"
 spaceship_git() {
   [[ $SPACESHIP_GIT_SHOW == false ]] && return
 
-  local git_branch="$(spaceship_git_branch)" git_status="$(spaceship_git_status)"
+  local git_branch="$(spaceship_git_branch)" 
+  local git_status="$(spaceship_git_status)"
+  local git_stat="$(spaceship_git_stat)"
 
   [[ -z $git_branch ]] && return
 
   spaceship::section \
     'white' \
     "$SPACESHIP_GIT_PREFIX" \
-    "${git_branch}${git_status}" \
+    "${git_branch}${git_status}${git_stat}" \
     "$SPACESHIP_GIT_SUFFIX"
 }
