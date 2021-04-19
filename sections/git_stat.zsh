237936b0b36887e168fa84e3d0b6a00375bbb495
Kasper <kasperliu129@gmail.com>
2021-04-19 16:39:25 +0800

    Fixed VAR

--- sections/git_stat.zsh
+++ sections/git_stat.zsh
@@ -8,29 +8,29 @@
 # Configuration
 # ------------------------------------------------------------------------------
 
-SPACESHIP_GIT_DIFFSTAT_SHOW="${SPACESHIP_GIT_DIFFSTAT_SHOW=true}"
-SPACESHIP_GIT_DIFFSTAT_PREFIX="${SPACESHIP_GIT_DIFFSTAT_PREFIX=" "}"
-SPACESHIP_GIT_DIFFSTAT_SUFFIX="${SPACESHIP_GIT_DIFFSTAT_SUFFIX=""}"
+SPACESHIP_GIT_STAT_SHOW="${SPACESHIP_GIT_STAT_SHOW=true}"
+SPACESHIP_GIT_STAT_PREFIX="${SPACESHIP_GIT_STAT_PREFIX=" "}"
+SPACESHIP_GIT_STAT_SUFFIX="${SPACESHIP_GIT_STAT_SUFFIX=""}"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-spaceship_git_diffstat() {
-  [[ $SPACESHIP_GIT_DIFFSTAT_SHOW == false ]] && return
+spaceship_git_stat() {
+  [[ $SPACESHIP_GIT_STAT_SHOW == false ]] && return
 
   spaceship::is_git || return
 
   # Shows count of lines added in green (with '+' prefix) and lines removed in red (with '-' prefix)
   # e.g. "+21 -7"
   
-  git_diffstat=$(command git diff --staged --numstat HEAD | awk 'NF==3 {added+=$1; removed+=$2} END {printf("\x1B[32m+%d\x1B[0m \x1B[31m-%d\x1B[0m\n", added, removed)}')
+  git_stat=$(command git diff --staged --numstat HEAD | awk 'NF==3 {added+=$1; removed+=$2} END {printf("\x1B[32m+%d\x1B[0m \x1B[31m-%d\x1B[0m\n", added, removed)}')
 
-  # Only show the diffstat if there are files staged.
+  # Only show the stat if there are files staged.
   if ! git diff-index --quiet HEAD; then
-    # Diffstats are colorized, so no color is necessary.
+    # stats are colorized, so no color is necessary.
     spaceship::section \
       '' \
-      "$SPACESHIP_GIT_DIFFSTAT_PREFIX$git_diffstat$SPACESHIP_GIT_DIFFSTAT_SUFFIX"
+      "$SPACESHIP_GIT_stat_PREFIX$git_stat$SPACESHIP_GIT_stat_SUFFIX"
   fi
 }
