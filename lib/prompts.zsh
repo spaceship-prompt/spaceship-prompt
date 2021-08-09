b46e6cb3d0693a53796686d3ad15aa44e500e323
Denys Dovhan <denysdovhan@gmail.com>
2021-08-09 21:00:44 +0300

    refactor(renderer): compose prompt from cache

--- lib/prompts.zsh
+++ lib/prompts.zsh
@@ -50,3 +50,32 @@ spaceship::ps2() {
   local char="${SPACESHIP_CHAR_SYMBOL_SECONDARY="$SPACESHIP_CHAR_SYMBOL"}"
   spaceship::section "$SPACESHIP_CHAR_COLOR_SECONDARY" "$char"
 }
+
+# Compose whole prompt from sections
+# USAGE:
+#   spaceship::compose_prompt [section...]
+spaceship::compose_prompt() {
+  # Option EXTENDED_GLOB is set locally to force filename generation on
+  # argument to conditions, i.e. allow usage of explicit glob qualifier (#q).
+  # See the description of filename generation in
+  # http://zsh.sourceforge.net/Doc/Release/Conditional-Expressions.html
+  setopt EXTENDED_GLOB LOCAL_OPTIONS
+
+  # Reset the first prefix value
+  _spaceship_prompt_opened="$SPACESHIP_PROMPT_FIRST_PREFIX_SHOW"
+
+  # Treat the first argument as list of prompt sections
+  # Compose whole prompt from diferent parts
+  for section in $@; do
+    spaceship::render_section "$(spaceship::get_cache $section)"
+  done
+}
+
+# Render the prompt. Compose variables using prompt functoins.
+# USAGE:
+#   spaceship::populate
+spaceship::populate() {
+  PROMPT='$(spaceship::prompt)'
+  RPROMPT='$(spaceship::rprompt)'
+  PS2='$(spaceship::ps2)'
+}
