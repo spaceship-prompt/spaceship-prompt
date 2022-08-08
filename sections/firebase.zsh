1ea7e94af8876dcfdada447f3964b5802b8c8d4d
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2022-08-09 02:03:07 +0300

    tracks firebase project target if inside a firebase project folder

---
+++ sections/firebase.zsh
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+SPACESHIP_FIREBASE_SHOW="${SPACESHIP_FIREBASE_SHOW=true}"
+SPACESHIP_FIREBASE_PREFIX="${SPACESHIP_FIREBASE_PREFIX="FB project: "}"
+SPACESHIP_FIREBASE_SYMBOL="${SPACESHIP_FIREBASE_SYMBOL="🔥 "}"
+SPACESHIP_FIREBASE_COLOR="${SPACESHIP_FIREBASE_COLOR="#d75f00"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# Shows active Firebase Project
+spaceship_firebase() {
+
+  # wheather to show this section
+  [[ $SPACESHIP_FIREBASE_SHOW == false ]] && return
+
+  # check if firebase-tools cli is installed
+  spaceship::exists firebase || return
+
+  local FBDIR=$(which firebase)
+
+  $FBDIR target 1>/dev/null
+  if [[ $? -eq 0 ]]; then
+    local target=$($FBDIR target | awk '{gsub(/\:/, "", $4)} {print $4}')
+  else
+    return
+  fi
+
+  spaceship::section \
+    "$SPACESHIP_FIREBASE_COLOR" \
+    "$SPACESHIP_FIREBASE_PREFIX" \
+    "${SPACESHIP_FIREBASE_SYMBOL}${target}" \
+    "$SPACESHIP_NODE_SUFFIX"
+}
