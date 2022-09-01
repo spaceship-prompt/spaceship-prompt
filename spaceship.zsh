068be43b4ebd9bfea82397a3ba52330e9d86d2f3
Denys Dovhan <denysdovhan@gmail.com>
2022-09-01 17:29:19 +0300

    refactor: move config loading to lib

--- spaceship.zsh
+++ spaceship.zsh
@@ -92,38 +92,6 @@ SPACESHIP_PROMPT_DEFAULT_PREFIX="${SPACESHIP_PROMPT_DEFAULT_PREFIX="via "}"
 SPACESHIP_PROMPT_DEFAULT_SUFFIX="${SPACESHIP_PROMPT_DEFAULT_SUFFIX=" "}"
 
 # ------------------------------------------------------------------------------
-# LOAD USER CONFIG
-# Try to load configuration file from pre-defined locations
-# ------------------------------------------------------------------------------
-
-if [[ -z "$SPACESHIP_CONFIG_PATH" ]]; then
-  SPACESHIP_CONFIG_PATH=(
-    "$HOME/.spaceshiprc"
-    "$HOME/.spaceshiprc.zsh"
-    "${XDG_CONFIG_HOME:="$HOME/.config"}/spaceship.zsh"
-    "${XDG_CONFIG_HOME:="$HOME/.config/spaceship"}/spaceship.zsh"
-    "${XDG_CONFIG_DIRS:="/etc/xdg"}/spaceship.zsh"
-  )
-fi
-
-export SPACESHIP_CONFIG
-
-# Find the first existing file if config is not overriden
-if [[ -z "$SPACESHIP_CONFIG" ]]; then
-  for config in $SPACESHIP_CONFIG_PATH; do
-    if [[ -f "$config" ]]; then
-      SPACESHIP_CONFIG="$config"
-      break
-    fi
-  done
-fi
-
-# Load file if it exists
-if [[ -f "$SPACESHIP_CONFIG" ]]; then
-  source "$SPACESHIP_CONFIG"
-fi
-
-# ------------------------------------------------------------------------------
 # LIBS
 # Spaceship utils/hooks/etc
 # ------------------------------------------------------------------------------
@@ -136,6 +104,7 @@ readonly SPACESHIP_LIBS=(
   "lib/core.zsh"    # Core functions for loading and rendering
   "lib/prompts.zsh" # Composing prompt variables
   "lib/cli.zsh"     # CLI interface
+  "lib/config.zsh"  # Loading Spaceship configuration file
   "lib/testkit.zsh" # Testing utils
 )
 
