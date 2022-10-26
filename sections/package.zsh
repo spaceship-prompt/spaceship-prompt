2bf181969d29c657fbdc1dfee10d5fa1a24d7076
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2022-10-26 11:46:10 +0200

    [Code] Support pyproject.toml for package section

--- sections/package.zsh
+++ sections/package.zsh
@@ -8,6 +8,7 @@
 #   * Cargo
 #   * Composer
 #   * Julia
+#   * Python (using pyproject.toml)
 
 # ------------------------------------------------------------------------------
 # Configuration
@@ -20,9 +21,10 @@ SPACESHIP_PACKAGE_PREFIX="${SPACESHIP_PACKAGE_PREFIX="is "}"
 SPACESHIP_PACKAGE_SUFFIX="${SPACESHIP_PACKAGE_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_PACKAGE_SYMBOL="${SPACESHIP_PACKAGE_SYMBOL="📦 "}"
 SPACESHIP_PACKAGE_COLOR="${SPACESHIP_PACKAGE_COLOR="red"}"
+SPACESHIP_PACKAGE_POETRY="${SPACESHIP_PACKAGE_POETRY=true}"
 
 if [ -z "$SPACESHIP_PACKAGE_ORDER" ]; then
-  SPACESHIP_PACKAGE_ORDER=(npm lerna cargo composer julia maven gradle)
+  SPACESHIP_PACKAGE_ORDER=(npm lerna cargo composer julia maven gradle python)
 fi
 
 # ------------------------------------------------------------------------------
@@ -105,6 +107,20 @@ spaceship_package::gradle() {
   local gradle_exe=$(spaceship::upsearch gradlew) || (spaceship::exists gradle && gradle_exe="gradle") || return
 
   $gradle_exe properties --no-daemon --console=plain -q 2>/dev/null | grep "^version:" | awk '{printf $2}'
+}
+
+spaceship_package::python() {
+
+  local pyproject_toml=$(spaceship::upsearch pyproject.toml) || return
+
+  if [[ "$SPACESHIP_PACKAGE_USE_POETRY" == false ]]; then
+    spaceship::datafile --toml $project_toml "project.version"
+  else
+    spaceship::datafile --toml $project_toml "tool.poetry.version"
+    if [[ $? != 0 ]]; then
+      spaceship::datafile --toml $project_toml "project.version"
+    fi
+  fi
 }
 
 # ------------------------------------------------------------------------------
