111c6f160c4376001d5469f8e8771ee89ea4158a
Alfred Bez <alfred.bez@googlemail.com>
2022-11-03 14:21:51 +0100

    fix: detect root via version variable (#1280)
    * Reload the SPACESHIP_ROOT if the directory does not exist (anymore)
    
    fixes #1140
    
    * Remove the 'next' branch from CONTRIBUTING.md
    
    * refactor: Do not use spaceship::root function
    
    * Reload the SPACESHIP_ROOT if the directory does not exist (anymore)
    
    fixes #1140
    
    * Remove the 'next' branch from CONTRIBUTING.md
    
    Co-authored-by: Denys Dovhan <denysdovhan@gmail.com>

--- spaceship.zsh
+++ spaceship.zsh
@@ -10,14 +10,17 @@
 # Useful for issue reporting
 export SPACESHIP_VERSION='4.7.0'
 
-# Determination of Spaceship working directory
-# https://git.io/vdBH7
-if [[ -z "$SPACESHIP_ROOT" ]]; then
+# Set SPACESHIP_ROOT if it isn't defined yet or if the directory does
+# not exist anymore (e.g. after an update to a newer version)
+# See https://github.com/spaceship-prompt/spaceship-prompt/pull/1280
+if [[ -z "$SPACESHIP_ROOT" || ! -d "$SPACESHIP_ROOT" ]]; then
+  # Determination of Spaceship working directory
+  # https://git.io/vdBH7
   if [[ "${(%):-%N}" == '(eval)' ]]; then
     if [[ "$0" == '-antigen-load' ]] && [[ -r "${PWD}/spaceship.zsh" ]]; then
       # Antigen uses eval to load things so it can change the plugin (!!)
       # https://github.com/zsh-users/antigen/issues/581
-      export -r SPACESHIP_ROOT=$PWD
+      export -r SPACESHIP_ROOT="$PWD"
     else
       print -P "%F{red}You must set SPACESHIP_ROOT to work from within an (eval).%f"
       return 1
@@ -26,7 +29,7 @@ if [[ -z "$SPACESHIP_ROOT" ]]; then
     # Get the path to file this code is executing in; then
     # get the absolute path and strip the filename.
     # See https://stackoverflow.com/a/28336473/108857
-    export -r SPACESHIP_ROOT=${${(%):-%x}:A:h}
+    export -r SPACESHIP_ROOT="${${(%):-%x}:A:h}"
   fi
 fi
 
