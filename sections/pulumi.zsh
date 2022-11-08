0434e03136335fd16da1f5cb721374a314fda562
Denys Dovhan <denysdovhan@gmail.com>
2022-11-08 20:42:41 +0200

    refactor(pulumi): Update to v4

--- sections/pulumi.zsh
+++ sections/pulumi.zsh
@@ -1,10 +1,10 @@
 #
-# PULUMI Stack
+# Pulumi
 #
-# PULUMI automates the repetitive tasks of setting up cloud resources
+# Pulumi automates the repetitive tasks of setting up cloud resources
 # Link: https://www.pulumi.com/
 #
-# This section shows you the current stack and number of resources 
+# This section shows you the current stack and number of resources
 # Link: https://www.pulumi.com/docs/intro/concepts/stack/
 
 # ------------------------------------------------------------------------------
@@ -12,9 +12,10 @@
 # ------------------------------------------------------------------------------
 
 SPACESHIP_PULUMI_SHOW="${SPACESHIP_PULUMI_SHOW=true}"
+SPACESHIP_PULUMI_ASYNC="${SPACESHIP_PULUMI_ASYNC=true}"
 SPACESHIP_PULUMI_PREFIX="${SPACESHIP_PULUMI_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
 SPACESHIP_PULUMI_SUFFIX="${SPACESHIP_PULUMI_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_PULUMI_SYMBOL="${SPACESHIP_PULUMI_SYMBOL="🛠𝓅 "}"
+SPACESHIP_PULUMI_SYMBOL="${SPACESHIP_PULUMI_SYMBOL=" "}"
 SPACESHIP_PULUMI_COLOR="${SPACESHIP_PULUMI_COLOR="133"}"
 
 # ------------------------------------------------------------------------------
@@ -27,14 +28,16 @@ spaceship_pulumi() {
   spaceship::exists pulumi || return
 
   # Show PULUMI Stack when exists
-  [[ -d .pulumi/stacks ]] || return
+  local pulumi_project=$(spaceship::upsearch Pulumi.y*ml)
+  [[ -n "$pulumi_project" || -d .pulumi/stacks ]] || return
 
-  local pulumi_stack=$(pulumi stack ls 2>/dev/null| sed -n -e '2p' |cut -f1 -d" "|sed s/\*// )
+  local pulumi_stack=$(pulumi stack ls 2>/dev/null | sed -n -e '2p' | cut -f1 -d" " | sed s/\*//)
   [[ -z $pulumi_stack ]] && return
 
   spaceship::section \
-    "$SPACESHIP_PULUMI_COLOR" \
-    "$SPACESHIP_PULUMI_PREFIX" \
-    "$SPACESHIP_PULUMI_SYMBOL$pulumi_stack" \
-    "$SPACESHIP_PULUMI_SUFFIX"
+    --color "$SPACESHIP_PULUMI_COLOR" \
+    --prefix "$SPACESHIP_PULUMI_PREFIX" \
+    --suffix "$SPACESHIP_PULUMI_SUFFIX" \
+    --symbol "$SPACESHIP_PULUMI_SYMBOL" \
+    "$pulumi_stack"
 }
