b10e093cad17691b8d32fca80ecbd8633cfc02bc
Denys Dovhan <denysdovhan@gmail.com>
2022-11-10 15:26:40 +0200

    fix(upsearch): Prevent globs from expanding to early
    commit dc0847a89b3a2afa609ab8f3fb8d312fa426e1e8
    Author: Denys Dovhan <denysdovhan@gmail.com>
    Date:   Thu Nov 10 15:23:31 2022 +0200
    
        fix(upsearch): Prevent upsearch globs from expanding
    
    commit 08f6c8d74c74dff1a92ce1f66ae9ab9f7cec34a4
    Author: Yevhen Karnaukhov <31127283+grimmyson@users.noreply.github.com>
    Date:   Thu Nov 10 10:55:43 2022 +0100
    
        Fixed showing java version in maven projects

--- sections/docker.zsh
+++ sections/docker.zsh
@@ -50,7 +50,8 @@ spaceship_docker() {
   local docker_context_section="$(spaceship::section::render $docker_context)"
 
   # Show Docker status only for Docker-specific folders or when connected to external host
-  local is_docker_project="$(spaceship::upsearch Dockerfile .devcontainer/Dockerfile docker-compose.y*ml)"
+  local docker_project_globs=('Dockerfile' '.devcontainer/Dockerfile' 'docker-compose.y*ml')
+  local is_docker_project="$(spaceship::upsearch Dockerfile $docker_project_globs)"
   [[ -n "$is_docker_project" || -f /.dockerenv || -n "$docker_context" ]] || return
 
   # if docker daemon isn't running you'll get an error saying it can't connect
--- sections/java.zsh
+++ sections/java.zsh
@@ -23,7 +23,8 @@ spaceship_java() {
   spaceship::exists java || return
 
   # Detect java project
-  local is_java_project="$(spaceship::upsearch pom.xml build.gradle* settings.gradle* build.xml)"
+  local java_project_globs=('pom.xml' 'build.gradle*' 'settings.gradle*' 'build.xml')
+  local is_java_project="$(spaceship::upsearch $java_project_globs)"
   [[ -n "$is_java_project" || -n *.(java|class|jar|war)(#qN^/) ]] || return
 
   # Extract java version
