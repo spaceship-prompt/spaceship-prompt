c5d83fea1eb3b54c4773c736658125413338bdbe
Yevhen Karnaukhov <ykarnaukhov@atlassian.com>
2022-11-14 22:13:48 +0100

    Added kotlin section

--- tests/kotlin.test.zsh
+++ tests/kotlin.test.zsh
@@ -12,22 +12,22 @@ oneTimeSetUp() {
   export TERM="xterm-256color"
   export PATH=$PWD/tests/stubs:$PATH
 
-  LUA_VERSION="5.4.4"
+  KOTLIN_VERSION="1.7.21"
 
   SPACESHIP_PROMPT_ASYNC=false
   SPACESHIP_PROMPT_FIRST_PREFIX_SHOW=true
   SPACESHIP_PROMPT_ADD_NEWLINE=false
-  SPACESHIP_PROMPT_ORDER=(lua)
+  SPACESHIP_PROMPT_ORDER=(kotlin)
 
   source "spaceship.zsh"
 }
 
 setUp() {
-  SPACESHIP_LUA_SHOW="true"
-  SPACESHIP_LUA_PREFIX="via "
-  SPACESHIP_LUA_SUFFIX=""
-  SPACESHIP_LUA_SYMBOL="🌙 "
-  SPACESHIP_LUA_COLOR="cyan"
+  SPACESHIP_KOTLIN_SHOW="true"
+  SPACESHIP_KOTLIN_PREFIX="via "
+  SPACESHIP_KOTLIN_SUFFIX=""
+  SPACESHIP_KOTLIN_SYMBOL="🅺 "
+  SPACESHIP_KOTLIN_COLOR="magenta"
 
   cd $SHUNIT_TMPDIR
 }
@@ -39,50 +39,28 @@ oneTimeTearDown() {
 }
 
 tearDown() {
-  unset SPACESHIP_LUA_SHOW
-  unset SPACESHIP_LUA_PREFIX
-  unset SPACESHIP_LUA_SUFFIX
-  unset SPACESHIP_LUA_SYMBOL
-  unset SPACESHIP_LUA_COLOR
+  unset SPACESHIP_KOTLIN_SHOW
+  unset SPACESHIP_KOTLIN_PREFIX
+  unset SPACESHIP_KOTLIN_SUFFIX
+  unset SPACESHIP_KOTLIN_SYMBOL
+  unset SPACESHIP_KOTLIN_COLOR
 }
 
 # ------------------------------------------------------------------------------
 # TEST CASES
 # ------------------------------------------------------------------------------
 
-test_lua_no_files() {
+test_kotlin_no_files() {
   local expected=""
   local actual="$(spaceship::testkit::render_prompt)"
   assertEquals "should not render without files" "$expected" "$actual"
 }
 
-test_lua_upsearch_file() {
-  FILES=(.lua-version)
+test_kotlin_file_extension() {
+  FILES=(first.kt second.kts)
   for file in $FILES; do
     touch $file
-    local expected="%{%B%}via %{%b%}%{%B%F{$SPACESHIP_LUA_COLOR}%}${SPACESHIP_LUA_SYMBOL}v${LUA_VERSION}%{%b%f%}"
-    local actual="$(spaceship::testkit::render_prompt)"
-    assertEquals "should render with file $file" "$expected" "$actual"
-    rm $file
-  done
-}
-
-test_lua_upsearch_dir() {
-  DIRS=(lua)
-  for dir in $DIRS; do
-    mkdir $dir
-    local expected="%{%B%}via %{%b%}%{%B%F{$SPACESHIP_LUA_COLOR}%}${SPACESHIP_LUA_SYMBOL}v${LUA_VERSION}%{%b%f%}"
-    local actual="$(spaceship::testkit::render_prompt)"
-    assertEquals "should render with dir $dir" "$expected" "$actual"
-    rm -rf $dir
-  done
-}
-
-test_lua_file_extension() {
-  FILES=(first.lua second.lua)
-  for file in $FILES; do
-    touch $file
-    local expected="%{%B%}via %{%b%}%{%B%F{$SPACESHIP_LUA_COLOR}%}${SPACESHIP_LUA_SYMBOL}v${LUA_VERSION}%{%b%f%}"
+    local expected="%{%B%}via %{%b%}%{%B%F{$SPACESHIP_KOTLIN_COLOR}%}${SPACESHIP_KOTLIN_SYMBOL}v${KOTLIN_VERSION}%{%b%f%}"
     local actual="$(spaceship::testkit::render_prompt)"
     assertEquals "should render with extension for file $file" "$expected" "$actual"
     rm $file
