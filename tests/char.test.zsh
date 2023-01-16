febab7f84a187bde475d2763b2985844895cbae9
Martin Hansen <dontbeevilpls@gmail.com>
2023-01-16 08:40:35 +0100

    fix(tests): not failing on char change
    The test case will not fail if the changing of char symbol feature is
    broken. We need to hardcode the expected output for that to be the case.
    
    This feature broke in ac313114c3da78d4ceb2b9b6a35822f764880e37

--- tests/char.test.zsh
+++ tests/char.test.zsh
@@ -27,6 +27,8 @@ setUp() {
   SPACESHIP_CHAR_COLOR_SUCCESS="green"
   SPACESHIP_CHAR_COLOR_FAILURE="red"
   SPACESHIP_CHAR_COLOR_SECONDARY="yellow"
+
+  cd $SHUNIT_TMPDIR
 }
 
 oneTimeTearDown() {
@@ -39,6 +41,8 @@ tearDown() {
   unset SPACESHIP_CHAR_PREFIX
   unset SPACESHIP_CHAR_SUFFIX
   unset SPACESHIP_CHAR_SYMBOL
+  unset SPACESHIP_CHAR_SYMBOL_SUCCESS
+  unset SPACESHIP_CHAR_SYMBOL_FAILURE
   unset SPACESHIP_CHAR_COLOR_SUCCESS
   unset SPACESHIP_CHAR_COLOR_FAILURE
   unset SPACESHIP_CHAR_COLOR_SECONDARY
@@ -48,11 +52,20 @@ tearDown() {
 # TEST CASES
 # ------------------------------------------------------------------------------
 
-test_char() {
+test_char_symbol() {
+  SPACESHIP_CHAR_SYMBOL='X '
+
+  local expected="%{%B%F{green}%}X %{%b%f%}"
+  local actual="$(spaceship::testkit::render_prompt)"
+
+  assertEquals "render char with custom symbol" "$expected" "$actual"
+}
+
+test_char_sucess() {
   SPACESHIP_CHAR_COLOR_SUCCESS=blue
   SPACESHIP_CHAR_SYMBOL_SUCCESS="S "
 
-  local expected="%{%B%F{$SPACESHIP_CHAR_COLOR_SUCCESS}%}$SPACESHIP_CHAR_SYMBOL_SUCCESS%{%b%f%}"
+  local expected="%{%B%F{blue}%}S %{%b%f%}"
   local actual="$(spaceship::testkit::render_prompt)"
 
   assertEquals "render char" "$expected" "$actual"
@@ -62,7 +75,7 @@ test_char_failure() {
   SPACESHIP_CHAR_COLOR_FAILURE=yellow
   SPACESHIP_CHAR_SYMBOL_FAILURE="F "
 
-  local expected="%{%B%F{$SPACESHIP_CHAR_COLOR_FAILURE}%}$SPACESHIP_CHAR_SYMBOL_FAILURE%{%b%f%}"
+  local expected="%{%B%F{yellow}%}F %{%b%f%}"
   command false # this command should exit with non-zero code
   local actual="$(spaceship::testkit::render_prompt)"
 
@@ -70,10 +83,10 @@ test_char_failure() {
 }
 
 test_char_prefix() {
-  SPACESHIP_CHAR_PREFIX='prefix'
+  SPACESHIP_CHAR_PREFIX='P '
   SPACESHIP_CHAR_SUFFIX=''
 
-  local expected="%{%B%}$SPACESHIP_CHAR_PREFIX%{%b%}%{%B%F{green}%}➜ %{%b%f%}"
+  local expected="%{%B%}P %{%b%}%{%B%F{green}%}➜ %{%b%f%}"
   local actual="$(spaceship::testkit::render_prompt)"
 
   assertEquals "render char with prefix" "$expected" "$actual"
@@ -81,9 +94,9 @@ test_char_prefix() {
 
 test_char_suffix() {
   SPACESHIP_CHAR_PREFIX=''
-  SPACESHIP_CHAR_SUFFIX='suffix'
+  SPACESHIP_CHAR_SUFFIX=' S'
 
-  local expected="%{%B%F{green}%}➜ %{%b%f%}%{%B%}$SPACESHIP_CHAR_SUFFIX%{%b%}"
+  local expected="%{%B%F{green}%}➜ %{%b%f%}%{%B%} S%{%b%}"
   local actual="$(spaceship::testkit::render_prompt)"
 
   assertEquals "render char with suffix" "$expected" "$actual"
