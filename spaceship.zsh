3fa7fb8893b5c1cae480e7412cd28cefb0457d24
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2024-10-06 19:00:40 +0200

    Add gleam to SPACESHIP_PROMPT_ORDER

--- spaceship.zsh
+++ spaceship.zsh
@@ -97,6 +97,7 @@ if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
     exit_code      # Exit code section
     sudo           # Sudo indicator
     char           # Prompt character
+    gleam          # Gleam section
   )
 fi
 
